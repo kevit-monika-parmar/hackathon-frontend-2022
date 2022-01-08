b83591483865772a9dd6c80c902ce2474c29768f
Monika Parmar <monika.parmar@kevit.io>
2022-01-08 16:33:49 +0530

    images added for bg

---
+++ src/helpers/images.js
@@ -0,0 +1,13 @@
+import ambulance from "../assets/images/ambulance-hospital.gif";
+// eslint-disable-next-line import/no-unresolved
+import add from "../assets/images/medical-add.gif";
+// eslint-disable-next-line import/no-unresolved
+import medical from "../assets/images/search-doctor.gif";
+
+const images = {
+  ambulance,
+  add,
+  medical,
+};
+
+export default images;
