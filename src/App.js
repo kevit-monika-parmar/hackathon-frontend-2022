f46f82d1bba0a3d4a62d9dd8adc78123f1be72bb
Monika Parmar <monika.parmar@kevit.io>
2022-01-08 19:47:43 +0530

    chat - demo created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
+import { useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 import images from "./helpers/images";
 
 function App() {
+  const { transcript, listening, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  const [value, setValue] = useState("");
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn&apos;t support speech recognition.</span>;
+  }
+  console.log(transcript);
+
+  const onSpeakerClick = (event) => {
+    setValue(transcript);
+    SpeechRecognition.startListening();
+    if (listening) {
+      SpeechRecognition.abortListening();
+    }
+    event.target.classList.toggle("record");
+  };
+
   return (
     <div className="bot">
-      <div className="images-first-row">
-        <img src={images.add} alt="add" />
-        <img src={images.ambulance} alt="ambulance" />
-        <img src={images.medical} alt="medical" />
-      </div>
       <div className="bot-container">
+        <div className="chat-container">
+          <div className="bubble user-chat-bubble">Kem cho</div>
+          <div className="bot-chat-bubble-container">
+            <div className="bubble bot-chat-bubble">Majaa ma chu.</div>
+          </div>
+        </div>
         <div className="bot-input">
-          <input placeholder="Type your query.." />
+          <input
+            placeholder="Type your query.."
+            onChange={(event) => setValue(transcript ?? event.target.value)}
+            value={value}
+          />
           <span className="material-icons-round send-icon" title="Send Message">
             send
           </span>
           <span
+            role="button"
             className="material-icons-round speaker-icon"
             title="Hold to send voice message"
+            onClick={onSpeakerClick}
+            onKeyPress={onSpeakerClick}
+            tabIndex={0}
           >
             keyboard_voice
           </span>
         </div>
+      </div>
+      <div className="images-row images-right-row">
+        <img src={images.cassie} alt="cassie" />
       </div>
     </div>
   );
