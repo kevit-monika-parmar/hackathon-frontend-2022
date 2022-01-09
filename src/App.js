292e9dff5cabdd3d996b597942ead7f4b0296d8f
Monika Parmar <monika.parmar@kevit.io>
2022-01-09 14:13:18 +0530

    socket.io added

--- src/App.js
+++ src/App.js
@@ -1,60 +1,173 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import SpeechRecognition, {
   useSpeechRecognition,
 } from "react-speech-recognition";
 import images from "./helpers/images";
+import botApiService from "./services/botApiService";
 
 function App() {
+  const inputRef = useRef();
+  const [chat, setChat] = useState([]);
+  const [botUserId, setBotUserId] = useState("");
+  const [conversationId, setConversationId] = useState("");
   const { transcript, listening, browserSupportsSpeechRecognition } =
     useSpeechRecognition();
-
   const [value, setValue] = useState("");
 
+  const speech = new SpeechSynthesisUtterance();
+  speech.text = value;
+  speech.pitch = 1;
+  speech.volume = 1;
+  // speech.lang = "en-US";
+  speech.rate = 1;
   if (!browserSupportsSpeechRecognition) {
-    return <span>Browser doesn&apos;t support speech recognition.</span>;
+    return <alert>Browser doesn&apos;t support speech recognition.</alert>;
   }
 
-  const onSpeakerClick = (event) => {
-    setValue(transcript);
+  const onSendClick = async () => {
+    const val = inputRef.current.value;
+    setValue(val);
+    const data = {
+      from: {
+        id: botUserId,
+      },
+      text: inputRef.current.value,
+      type: "message",
+    };
+    setChat([...chat, { from: "user", message: inputRef.current.value }]);
+    const botMessageResponse = await botApiService.activitySend(
+      data,
+      conversationId
+    );
+    setChat([
+      ...chat,
+      { from: "bot", message: botMessageResponse?.data?.text },
+    ]);
+    speechSynthesis.speak(botMessageResponse?.data?.text);
+  };
+
+  const onInputKeyPress = async (e) => {
+    if (e.key === "Enter" && e.target.value.toString().trim().length > 0) {
+      setValue(e.target.value);
+      setChat([...chat, { from: "user", message: e.target.value }]);
+      const data = {
+        from: {
+          id: botUserId,
+        },
+        text: value,
+        type: "message",
+      };
+      setChat([...chat, { from: "bot", message: "..." }]);
+      const botMessageResponse = await botApiService.activitySend(
+        data,
+        conversationId
+      );
+      if (botMessageResponse) {
+        chat.pop();
+        setChat([
+          ...chat,
+          { from: "bot", message: botMessageResponse?.data?.text },
+        ]);
+      }
+      speechSynthesis.speak(botMessageResponse?.data?.text);
+    }
+  };
+
+  const onSpeakerClick = async (event) => {
     SpeechRecognition.startListening();
     if (listening) {
-      SpeechRecognition.abortListening();
+      SpeechRecognition.stopListening();
     }
     event.target.classList.toggle("record");
+    const data = {
+      from: {
+        id: botUserId,
+      },
+      text: value,
+      type: "message",
+      language: "en",
+    };
+    await botApiService.activitySend(data, conversationId);
   };
+
+  const userChat = (userMessage) => (
+    <div className="bubble user-chat-bubble">{userMessage}</div>
+  );
+
+  const botChat = (message) => (
+    <div className="bot-chat-bubble-container">
+      <div className="bubble bot-chat-bubble">{message}</div>
+    </div>
+  );
+
+  useEffect(async () => {
+    const res = await botApiService.getBotConversations();
+    //  console.log(res?.data?.data);
+    setBotUserId(res?.data?.data?.botUserId);
+    setConversationId(res?.data?.data?.conversationId);
+
+    const socketUrl = res?.data?.data?.streamUrl;
+    const socket = new WebSocket(socketUrl);
+    socket.onopen = () => {
+      //  console.log("Socket connected");
+    };
+    socket.onmessage = (e) => {
+      console.log(e, "hfhfhfgh");
+    };
+  }, []);
+
+  useEffect(() => {
+    if (transcript?.toString().trim().length > 0) {
+      console.log(transcript);
+      setValue(transcript);
+    }
+  }, [transcript]);
 
   return (
     <div className="bot">
-      <div className="bot-container">
-        <div className="chat-container">
-          <div className="bubble user-chat-bubble">Kem cho</div>
-          <div className="bot-chat-bubble-container">
-            <div className="bubble bot-chat-bubble">Majaa ma chu.</div>
+      <div className="bot-content-container">
+        <div className="bot-container">
+          <div className="bot-header">Talk to Cassie</div>
+          <div className="chat-container">
+            {chat.length > 0 &&
+              chat.map((message) =>
+                message.from === "user"
+                  ? userChat(message.message)
+                  : botChat(message.message)
+              )}
+          </div>
+          <div className="bot-input">
+            <input
+              ref={inputRef}
+              placeholder="Type your query.."
+              onChange={(event) => setValue(event.target.value)}
+              onKeyPress={onInputKeyPress}
+              value={value ?? transcript}
+            />
+            <span
+              role="button"
+              className="material-icons-round send-icon"
+              title="Send Message"
+              onClick={onSendClick}
+              onKeyPress={onSendClick}
+              tabIndex={0}
+            >
+              send
+            </span>
+            <span
+              role="button"
+              className="material-icons-round speaker-icon"
+              title="Hold to send voice message"
+              onClick={onSpeakerClick}
+              onKeyPress={onSpeakerClick}
+              tabIndex={-1}
+            >
+              keyboard_voice
+            </span>
           </div>
         </div>
-        <div className="bot-input">
-          <input
-            placeholder="Type your query.."
-            onChange={(event) => setValue(transcript ?? event.target.value)}
-            value={value}
-          />
-          <span className="material-icons-round send-icon" title="Send Message">
-            send
-          </span>
-          <span
-            role="button"
-            className="material-icons-round speaker-icon"
-            title="Hold to send voice message"
-            onClick={onSpeakerClick}
-            onKeyPress={onSpeakerClick}
-            tabIndex={0}
-          >
-            keyboard_voice
-          </span>
-        </div>
-      </div>
-      <div className="images-row images-right-row">
-        <img src={images.cassie} alt="cassie" />
+
+        <img src={images.cassie} className="cassie" alt="cassie" />
       </div>
     </div>
   );
