16441b9e06e23985d31ccf1459072488c38a17ba
Monika Parmar <monika.parmar@kevit.io>
2022-01-09 14:22:32 +0530

    ngrok changed

--- src/constants/urlConstants.js
+++ src/constants/urlConstants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://ddd3-43-241-145-91.ngrok.io/";
+export const BASE_URL = "https://72ce-43-241-145-91.ngrok.io/";
 
 export const BOT_URLS = {
   CONVERSATIONS: "webchat/conversations/",
